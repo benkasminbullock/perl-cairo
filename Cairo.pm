4e159eed8d6aa5128f052091364d3d6e1ba30f13
Torsten Schönfeld <tsch@src.gnome.org>
2008-02-12 20:18:09 +0000

    Cairo.pm Unstable release 1.052.
    	* Cairo.pm
    	* NEWS: Unstable release 1.052.
    
    svn path=/trunk/; revision=147

--- Cairo.pm
+++ Cairo.pm
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2004-2007 by the cairo perl team (see the file README)
+# Copyright (c) 2004-2008 by the cairo perl team (see the file README)
 #
 # Licensed under the LGPL, see LICENSE file for more information.
 #
@@ -14,7 +14,7 @@ use DynaLoader;
 
 our @ISA = qw/DynaLoader/;
 
-our $VERSION = '1.051';
+our $VERSION = '1.052';
 
 sub dl_load_flags { $^O eq 'darwin' ? 0x00 : 0x01 }
 
@@ -1480,6 +1480,6 @@ Contains the reference manual
 
 =head1 COPYRIGHT
 
-Copyright (C) 2004-2007 by the cairo perl team
+Copyright (C) 2004-2008 by the cairo perl team
 
 =cut
